cb6ea658e0e490cc08cf7b46d1a90a8517768425
Ardiansky <permanadion113@gmail.com>
2024-12-31 15:22:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,31 +8,49 @@ const missionRouter = require('./routes/missionRoute');
 const app = express();
 const PORT = 8000;
 
-const whiteListCors = ['https://www.jkt48showroom.com/', 'http://localhost:3000']
+const whiteListCors = [
+    'https://www.jkt48showroom.com',
+    'http://localhost:3000'
+];
 
+// Middleware CORS
 app.use(cors({
-    origin: function(origin, cb) {
-        if(whiteListCors.includes(origin)) {
-            cb(null, true)
+    origin: function (origin, cb) {
+        if (!origin || whiteListCors.includes(origin)) {
+            cb(null, true);
         } else {
-            cb(new Error('Not allowed'))
+            cb(new Error('Not allowed by CORS'));
         }
     }
-}))
+}));
+
+// Middleware Body Parser
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-    console.log(`Server Running on port http://localhost:${PORT}`);
+// Error Handler untuk CORS
+app.use((err, req, res, next) => {
+    if (err.message === 'Not allowed by CORS') {
+        res.status(403).send({ error: 'Origin not allowed by CORS policy' });
+    } else {
+        next(err);
+    }
 });
 
+// Route Utama
 app.get('/', (req, res) => {
     res.send({
         message: 'Welcome To JKT48 SHOWROOM API',
         author: 'https://github.com/ikhbaldwiyan',
         repository: 'https://github.com/ikhbaldwiyan/jkt48-showroom-api'
-    })
+    });
 });
 
+// Router lainnya
 app.use('/api/rooms', roomRouter);
 app.use('/api/lives', liveRouter);
 app.use('/api/missions', missionRouter);
+
+// Menjalankan Server
+app.listen(PORT, () => {
+    console.log(`Server Running on port http://localhost:${PORT}`);
+});
